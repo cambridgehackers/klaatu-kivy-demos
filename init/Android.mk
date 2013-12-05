b4bc265c6cb33977c9ad31df5fd0f8642d47aa05
Greg Bentley <greg.bentley@nokia.com>
2013-12-05 13:59:01 -0800

    Fixed ordering issue.

--- init/Android.mk
+++ init/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 file := $(TARGET_ROOT_OUT)/init.klaatu-kivy.rc
-$(file) : $(LOCAL_PATH)/init.klaatu-kivy.rc | $(TARGET_ROOT_OUT)/init.klaatu.rc $(ACP)
+$(file) : $(LOCAL_PATH)/init.klaatu-kivy.rc | $(TARGET_ROOT_OUT)/init.klaatu.rc $(TARGET_ROOT_OUT)/init.klaatu-headless.rc $(ACP)
 	$(transform-prebuilt-to-target)
 	echo "import /init.klaatu-kivy.rc" >> $(TARGET_ROOT_OUT)/init.klaatu.rc
 	mkdir -p $(TARGET_OUT_DATA)/property

d9dd79ac2572901098c496f3bc81d64817c16b62
midesimo <@>
2014-01-16 18:12:25 -0800

    First commit of kivy launcher.
    Images from:
    
    https://www.flickr.com/photos/worldoflard/13565494913/
    "a view across time" by worldoflard
    
    https://www.flickr.com/photos/worldoflard/13608431135/
    "the elements align" by worldoflard

---
+++ README.md
@@ -0,0 +1,8 @@
+# Image sources #
+
+https://www.flickr.com/photos/worldoflard/13565494913/
+"a view across time" by worldoflard
+    
+https://www.flickr.com/photos/worldoflard/13608431135/
+"the elements align" by worldoflard
+
---
+++ kvc-launcher/launcher.py
@@ -0,0 +1,36 @@
+'''
+KivyCat Launcher
+========
+'''
+
+'''
+import kivy
+kivy.require('1.0.7')
+'''
+
+import os.path
+from kivy.app import App, Widget
+from kivy.uix.carousel import Carousel
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.lang import Builder
+from touchtracer import Touchtracer, TouchtracerApp
+
+class KlaatuImage(Image):
+    pass
+
+class KlaatuLabel(Label):
+    pass
+
+class Launcher(Carousel):
+    def run_touchtracer(self):
+        w = Builder.load_file(os.path.dirname(os.path.realpath(__file__)) + "/touchtracer/touchtracer.kv")
+        Window.remove_widget(self)
+        Window.add_widget(Touchtracer())
+
+class LauncherApp(App):
+    pass
+
+if __name__ == '__main__':
+    LauncherApp().run()

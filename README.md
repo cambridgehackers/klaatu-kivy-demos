e095f5689f5b4f2b63053c3d2388b68b866b5b96
midesimo <@>
2014-01-20 19:18:06 -0800

    New launcher updates, README.
    Updates to the launcher:
    1. New overall design, Carousel-free
    2. Goal: no need to modify apps
    3. Gorgeous transitions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Kivy Example Apps #
+This repository contains few example Kivy applications meant to show the potential of the platform.
+
+## How to run ##
+With a working Kivy environment, launching the applications should prove straightforward. Just `python launcher.py` to run the launcher.
+
 # Image sources #
 
 https://www.flickr.com/photos/worldoflard/13565494913/
@@ -5,4 +11,3 @@ https://www.flickr.com/photos/worldoflard/13565494913/
     
 https://www.flickr.com/photos/worldoflard/13608431135/
 "the elements align" by worldoflard
-
--- kc-launcher/launcher.py
+++ kc-launcher/launcher.py
@@ -1,21 +1,22 @@
 '''
 KivyCat Launcher
-========
-'''
-
-'''
-import kivy
-kivy.require('1.0.7')
+================
 '''
 
 import os.path
+import kivy
 from kivy.app import App, Widget
-from kivy.uix.carousel import Carousel
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.image import Image
-from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition
 from kivy.lang import Builder
 from apps.touchtracer import Touchtracer, TouchtracerApp
+
+kivy.require('1.7.0')
+
+class KlaatuButton(Button):
+    pass
 
 class KlaatuImage(Image):
     pass
@@ -23,14 +24,25 @@ class KlaatuImage(Image):
 class KlaatuLabel(Label):
     pass
 
-class Launcher(Carousel):
-    def run_touchtracer(self):
-        w = Builder.load_file(os.path.dirname(os.path.realpath(__file__)) + "/apps/touchtracer/touchtracer.kv")
-        Window.remove_widget(self)
-        Window.add_widget(Touchtracer())
+class Launcher(Screen):
+    pass
+
+class LauncherScreen(Screen):
+    pass
 
 class LauncherApp(App):
-    pass
+    def build_screen_touchtracer(self):
+        # Build the Touchtracer demo app
+        touchtracer = Builder.load_file(os.path.dirname(os.path.realpath(__file__)) + "/apps/touchtracer/touchtracer.kv")
+        touchtracer_screen = Screen(name = 'Touchtracer')
+        touchtracer_screen.add_widget(Touchtracer())
+        return touchtracer_screen
+
+    def build(self):
+        root = ScreenManager(transition = SwapTransition(duration = .75))
+        root.add_widget(LauncherScreen(name = 'Main'))
+        root.add_widget(self.build_screen_touchtracer())
+        return root
 
 if __name__ == '__main__':
     LauncherApp().run()

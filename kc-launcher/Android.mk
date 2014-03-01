8543803118a501bb82e48f2a4a56b10dbba8ba27
Greg Bentley <greg.bentley@nokia.com>
2014-02-28 23:53:31 -0700

    Combined Android.mk files into one.

--- kc-launcher/Android.mk
+++ kc-launcher/Android.mk
@@ -3,13 +3,25 @@ include $(CLEAR_VARS)
 
 launcher_dir := $(TARGET_OUT_SHARED_LIBRARIES)/python2.7/kc-launcher
 
-file := $(launcher_dir)/kc-launcher
-$(file): srcdir := $(LOCAL_PATH)
-$(file): dstdir := $(launcher_dir)
-$(file) : $(LOCAL_PATH)/*.py $(LOCAL_PATH)/*.kv $(LOCAL_PATH)/*.jpg | $(ACP)
+app := kc-launcher
+$(app): srcdir := $(LOCAL_PATH)
+$(app): dstdir := $(launcher_dir)
+$(app) : $(LOCAL_PATH)/*.py $(LOCAL_PATH)/*.kv $(LOCAL_PATH)/*.jpg | $(ACP)
 	@mkdir -p $(dstdir)
-	$(ACP) $(srcdir)/*.kv $(srcdir)/*.jpg $(srcdir)/*.py $(dstdir)
+	$(ACP) $^ $(dstdir)
 	@mkdir -p $(dstdir)/apps
 	$(ACP) $(srcdir)/apps/*.py $(dstdir)/apps
-ALL_MODULES += $(file)
-ALL_DEFAULT_INSTALLED_MODULES += $(file)
+ALL_MODULES += $(app)
+ALL_DEFAULT_INSTALLED_MODULES += $(app)
+.PHONY: $(app)
+
+app := touchtracer
+appdir := $(LOCAL_PATH)/apps/$(app)
+$(app): srcdir := $(LOCAL_PATH)/apps/touchtracer
+$(app): dstdir := $(launcher_dir)/apps/touchtracer
+$(app) : $(appdir)/*.py $(appdir)/*.kv $(appdir)/*.png | $(ACP)
+	@mkdir -p $(dstdir)
+	$(ACP) $^ $(dstdir)
+ALL_MODULES += $(app)
+ALL_DEFAULT_INSTALLED_MODULES += $(app)
+.PHONY: $(app)

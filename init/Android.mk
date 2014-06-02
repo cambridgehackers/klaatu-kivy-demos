f74b82ac5688b51a84461b9c44994a5a0d769494
Don Mahurin <@>
2014-06-02 11:17:26 -0700

    persist.sys.ui.config is set in klaatu-initscripts. remove headless.rc dependency

--- init/Android.mk
+++ init/Android.mk
@@ -2,11 +2,9 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 file := $(TARGET_ROOT_OUT)/init.klaatu-kivy.rc
-$(file) : $(LOCAL_PATH)/init.klaatu-kivy.rc | $(TARGET_ROOT_OUT)/init.klaatu.rc $(TARGET_ROOT_OUT)/init.klaatu-headless.rc $(ACP)
+$(file) : $(LOCAL_PATH)/init.klaatu-kivy.rc | $(TARGET_ROOT_OUT)/init.klaatu.rc $(ACP)
 	$(transform-prebuilt-to-target)
 	echo "import /init.klaatu-kivy.rc" >> $(TARGET_ROOT_OUT)/init.klaatu.rc
-	mkdir -p $(TARGET_OUT_DATA)/property
-	echo -n kivy > $(TARGET_OUT_DATA)/property/persist.sys.ui.config
 	echo "kivy Python/Kivy" >> $(TARGET_ROOT_OUT)/UIs.txt
 ALL_MODULES += $(file)
 ALL_DEFAULT_INSTALLED_MODULES += $(file)
